a91e619f9c29c07dd967b56a1a0a4519268d6953
ginozh <hqzhang1983@gmail.com>
2016-12-21 23:44:26 +0800

    2.76 add VisulEffect

---
+++ ffmpeg-3.0.2/sh_configure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+<<COMMENT
+@author: ginozhang
+@date: 2016-12-21
+@description: 
+COMMENT
+#frei0r: https://ffmpeg.zeranoe.com/forum/viewtopic.php?f=5&t=312&p=2716#p2716
+./configure --prefix=/mingw32 --target-os=mingw32 --arch=i686 --enable-debug --disable-static --enable-avresample --enable-dxva2 --enable-d3d11va --enable-fontconfig --enable-gnutls --enable-gpl --enable-libass --enable-libbluray --enable-libcaca --enable-libcelt --enable-libdcadec --enable-libfreetype --enable-libgsm --enable-libmodplug --enable-libmp3lame --enable-libopencore_amrnb --enable-libopencore_amrwb --enable-libopus --enable-librtmp --enable-libschroedinger --enable-libspeex --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libx265 --enable-libxvid --enable-libvpx --enable-openal --enable-libwavpack --enable-pic --enable-postproc --enable-runtime-cpudetect --enable-shared --enable-static --enable-swresample --enable-version3 --enable-zlib --disable-doc --disable-pthreads --disable-pthreads --disable-w32threads --disable-os2threads --enable-frei0r
