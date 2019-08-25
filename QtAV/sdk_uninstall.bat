3fe4c435170868738485e017482d5220eacc04b2
ginozh <hqzhang1983@gmail.com>
2019-08-25 16:53:04 +0800

    add qtav

---
+++ QtAV/sdk_uninstall.bat
@@ -0,0 +1,13 @@
+rm -f C:\msys64\mingw64\lib\*Qt*AV*
+rm -f C:\msys64\mingw64\lib\libQt5AV.a
+rd /s /q C:\msys64\mingw64\include\QtAV
+rm -f C:\msys64\mingw64\share\qt5\mkspecs\features\av.prf
+rm -f C:\msys64\mingw64\share\qt5\mkspecs\modules\qt_lib_av*.pri
+rm -f C:\msys64\mingw64\bin\Qt*AV*.dll
+rm -f C:\msys64\mingw64\lib\*Qt*AV*
+rm -f C:\msys64\mingw64\lib\libQt5AVWidgets.a
+rd /s /q C:\msys64\mingw64\include\QtAVWidgets
+rm -f C:\msys64\mingw64\share\qt5\mkspecs\features\avwidgets.prf
+rm -f C:\msys64\mingw64\share\qt5\mkspecs\modules\qt_lib_avwidgets*.pri
+rm -f C:\msys64\mingw64\bin\Qt*AV*.dll
+rd /s /q C:\msys64\mingw64\share\qt5\qml\QtAV
